0bdb337f1a5c3ee827153e14ceb5291478c0e80c
Harry Steele <harrybsteele@gmail.com>
2023-05-19 17:04:32 -0400

    input works

--- src/Weather.js
+++ src/Weather.js
@@ -1,24 +1,58 @@
 import React from "react";
-import { GetWeather } from "./Functions";
 import { useEffect, useState } from "react";
+import axios from "axios";
+
+const baseURL =
+  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline";
+const apiKey = "T7YZ8RPUW3YW3ZF9NYQGSKT8W";
 
 const Weather = () => {
-    const [location, setLocation] = useState("")
-    const [data, setData] = useState([]);
+  const [location, setLocation] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  const [savedLocations, setSavedLocations] = useState([])
 
-    useEffect(() => {
-       if(location !== "") {
-        setData(GetWeather(location))
-       };
-    })
+//   resolvedAddress
 
-    return (
-        <>
-        <h1>Weather</h1>
-        <input type="text" name="location"></input>
-        <button>Submit</button>
-        </>
-    )
+// const checkForDoubles = () => {
+//     savedLocations.some(city => city.resolvedAddress === data.resolvedAddress) ? alert("You can't add the same location twice")
+// }
+
+const removeDuplicates = (newValue, arr) => {
+    return [...newValue, Set(arr)]
 }
 
-export default Weather
+  console.log(savedLocations)
+
+  useEffect(() => {
+    function getWeather() {
+      const response = fetch(`${baseURL}/${location}?key=${apiKey}`)
+        .then((result) => result.json())
+        .then((data) => setSavedLocations(savedLocations => [savedLocations, data])).then(setLocation(""))
+    }
+
+    if (location !== "") {
+      getWeather();
+    }
+  }, [submitted]);
+
+  const submit = (e) => {
+    e.preventDefault();
+    setSubmitted(!submitted);
+  };
+
+  const handleChange = (e) => {
+    setLocation(e.target.value);
+  };
+
+  return (
+    <>
+      <h1>Weather</h1>
+      <form onSubmit={submit}>
+        <input type="text" name="location" onChange={handleChange} value={location}></input>
+        <button>Submit</button>
+      </form>
+    </>
+  );
+};
+
+export default Weather;
